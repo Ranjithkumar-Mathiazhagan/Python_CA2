b8e89829f325cd7fd26df5d3e261a5f9aedc188f
Ranjithkumar-Mathiazhagan <ranjithvoc32@gmail.com>
2024-07-15 19:51:37 +0100

    updated home route

--- Car_service_booking/app.py
+++ Car_service_booking/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template,  redirect, url_for, request, flash, session
 
 
 app = Flask (__name__)
 
 
 @app.route("/")
-def home():
-    return "home page"
+def index():
+    return render_template ("index.html")
 
 
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
